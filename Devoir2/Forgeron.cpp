6b2f7537ce35505ba805b86f2fd1dea3987b27cc
simonduchesne <simsimondu@gmail.com>
2019-11-13 14:31:17 -0500

    ajout des recette dans chaque construction possible représenté par des const int. ajour de l'objet forgeron qui est vide pour le moment, mais c'est lui qui sera notre banquier pour l'alogorithme du banquier. le forgeron est une objet puisqu'elle est une femme. et ouais les femme aussi peuvent etre des forgerons bande de mysogines.

---
+++ Devoir2/Forgeron.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Simon Duchesne on 2019-11-13.
+//
+
+#include "Forgeron.h"
---
+++ Devoir2/Forgeron.h
@@ -0,0 +1,14 @@
+//
+// Created by Simon Duchesne on 2019-11-13.
+//
+
+#ifndef DEVOIR2_FORGERON_H
+#define DEVOIR2_FORGERON_H
+
+
+class Forgeron {
+
+};
+
+
+#endif //DEVOIR2_FORGERON_H
